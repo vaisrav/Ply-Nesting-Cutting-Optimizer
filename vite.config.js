8e3312298e212f3c3646e3d25610100069204a9f
vaisrav <vaisrav@gmail.com>
2026-09-21 20:18:55 -0400

    changes

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/ply-calc/',
+  base: '/Ply-Nesting-Cutting-Optimizer/',
 })
